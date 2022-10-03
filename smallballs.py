e21de29e32b6144ad662d4a1cc4a79ffb9203e29
Eclipse/Alex <90584863+LunarEclipses@users.noreply.github.com>
2022-10-02 20:08:41 -0400

    Update smallballs.py

--- smallballs.py
+++ smallballs.py
@@ -6,19 +6,21 @@ import os
 
 
 files = []
-
-for file in os.listdir():
-    if file == "bigballs.py" or file=="thekey.key" or file=="smallballs.py":
-        continue
-    if os.path.isfile(file):
-        files.append(file)
-
-with open("thekey.key", "rb") as key:
-    secretkey=key.read()
+def main():
+    for file in os.listdir():
+        if file == "bigballs.py" or file=="thekey.key" or file=="smallballs.py":
+            continue
+        if os.path.isfile(file):
+            files.append(file)
     
-for file in files:
-    with open(file, "rb") as thefile:
-        contents = thefile.read()
-    contents_decrypted = Fernet(secretkey).decrypt(contents)
-    with open(file, "wb") as thefile:
-        thefile.write(contents_decrypted)
+    with open("thekey.key", "rb") as key:
+        secretkey=key.read()
+    
+    for file in files:
+        with open(file, "rb") as thefile:
+            contents = thefile.read()
+        contents_decrypted = Fernet(secretkey).decrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_decrypted)
+if __name__ = '__main__':
+    main()
